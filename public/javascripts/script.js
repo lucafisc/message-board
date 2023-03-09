e5e8e7c829bf3f175fa776d6ee2f131e81057b9b
lucafisc <lucafischer@fastmail.com>
2023-03-09 11:59:47 +0100

    Add files

---
+++ public/javascripts/script.js
@@ -0,0 +1,12 @@
+console.log("aaaaaa");
+window.onload = () => {
+	console.log("scrolling");
+  const chatbox = document.querySelector(".chatbox");
+  chatbox.scrollTop = chatbox.scrollHeight;
+}
+
+const form = querySelector("#form");
+
+form.addEventListener('submit', () => {
+	const name = querySelector
+})
